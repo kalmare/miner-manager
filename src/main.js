56622f0566910a867f99c18ccdfa96ae0909d36c
kalmare <kalmare01@gmail.com>
2017-12-12 01:19:30 +0900

    [Change] Menu is invisible

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, shell, Menu} = require('electron');
 const path = require('path');
 const url = require('url');
 
 let window;
 
 function createWindow() {
-    window = new BrowserWindow({width:800, height:600});
     window.loadURL(__dirname+'/../build/index.html');
+
+    Menu.setApplicationMenu(null);
 
     window.on('closed', () => {
         window = null;
