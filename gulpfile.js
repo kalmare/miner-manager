2ef55809f715fd2ae7271804098a21afe333cf02
kalmare <kalmare01@gmail.com>
2017-12-12 23:52:59 +0900

    [Add] packaging task of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const replace = require('gulp-replace');
 const electron = require('electron-connect').server.create();
+const packager = require('electron-packager');
 
 const backend = 'src';
 const frontend = 'view';
@@ -36,4 +38,21 @@ gulp.task('start', ['compile'], () => {
     gulp.watch(frontend+'/**/*.{html,css,js}', ['compile', electron.reload]);
 
     gulp.watch(backend+'/**/*.js', electron.restart);
+});
+
+gulp.task('remove_debugger', ['compile'], () => {
+    return gulp.src(build+'/index.js')
+        .pipe(replace(/require\('electron-connect'\).client.create\(\);/g), '')
+        .pipe(gulp.dest(build));
+});
+
+gulp.task('package', ['remove_debugger'], () => {
+    return packager({
+        dir: './',
+        out: 'release/',
+        name: 'Bitzeny Miner Manager',
+        arch: 'x64',
+        platform: 'win32',
+        overwrite: true
+    });
 });
